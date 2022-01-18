5a74c7dff07ce7ad3ed89747f472eae40f81e9fe
Kira Selby <kaselby@uwaterloo.ca>
2022-01-18 18:26:40 -0500

    process adsorbates

--- scripts/process_adsorbates.py
+++ scripts/process_adsorbates.py
@@ -34,6 +34,7 @@ def download_ads(root_dir, ads_num):
     filename = os.path.join(root_dir, os.path.basename(download_link))
     logging.info("Extracting contents...")
     os.system(f"tar -xvf {filename} -C {root_dir}")
+    os.remove(filename)
 
 def read_trajectory_extract_features(a2g, traj_path):
     traj = ase.io.read(traj_path, ":")
@@ -44,8 +45,8 @@ def read_trajectory_extract_features(a2g, traj_path):
     data_objects[1].tags = torch.LongTensor(tags)
     return data_objects
 
-def write_ads_to_lmdb(ads_dir, ads_num):
-    with open(os.path.join(ads_dir, "system.txt")) as sysfile:
+def write_ads_to_lmdb(root_dir, ads_num):
+    with open(os.path.join(root_dir, str(ads_num), "system.txt")) as sysfile:
         system_data = sysfile.readlines()
     ref_energies = {}
     for line in system_data:
@@ -53,7 +54,7 @@ def write_ads_to_lmdb(ads_dir, ads_num):
         id = int(re.findall(r'random([\d]*)', id)[0])
         ref_energies[int(id)] = float(energy)
     
-    lmdb_path = os.path.join(ads_dir, str(ads_num) + ".lmdb")
+    lmdb_path = os.path.join(root_dir, str(ads_num) + ".lmdb")
     db = lmdb.open(
         lmdb_path,
         map_size=1099511627776 * 2,
@@ -71,9 +72,9 @@ def write_ads_to_lmdb(ads_dir, ads_num):
         r_fixed=True,
     )
 
-    traj_folder = os.path.join(ads_dir, str(ads_num)+"_uncompressed")
+    traj_folder = os.path.join(root_dir, str(ads_num), str(ads_num)+"_uncompressed")
     ads_files = glob.glob(os.path.join(traj_folder, "*.extxyz"))
-    for file in ads_files:
+    for file in tqdm.tqdm(ads_files):
         system_id = int(re.findall(r'random([\d]*)', file)[0])
         ref_energy = ref_energies[system_id]
 
@@ -87,6 +88,7 @@ def write_ads_to_lmdb(ads_dir, ads_num):
         initial_struc.pos_relaxed = relaxed_struc.pos
 
         initial_struc.sid = system_id
+        initial_struc.ads_num=ads_num
 
         if initial_struc.edge_index.shape[1] == 0:
             print("no neighbors", system_id)
@@ -113,4 +115,15 @@ def process_adsorbates(root_dir, N_ADS=82):
             shutil.rmtree(compressed_dir)
 
         write_ads_to_lmdb(ads_dir, i)
+        shutil.rmtree(ads_dir)
+
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--root_dir', type=str, default="adsorbate-data")
+
+args = parser.parse_args()
+
+process_adsorbates(args.root_dir)
 
